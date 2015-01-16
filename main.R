0ad2406ea04fa7d6ac603eb31337bc69e16ab095
David <davidscholtealbers@gmail.com>
2015-01-16 01:25:51 +0100

    Draft version of the main model

--- main.R
+++ main.R
@@ -1,48 +1,88 @@
-Create a model object using lm() using Landsat band reflectance values as predictors for tree cover (VCF). Using the resulting model object, predict VCF values for the Gewata area.
+# Team AD-Scripting: Astrid Bos & David Scholte-Albers
+# Friday 16-01-2015
+# Exercise 7: Advanced Raster Analysis
 
-
-
-produce one or more plots that demonstrate the relationship between the Landsat bands and the VCF tree cover. What can you conclude from this/these plot(s)?
-create an lm() model and show a summary (e.g. using summary()) of the model object you created. Which predictors (bands) are probably most important in predicting tree cover?
-plot the predicted tree cover raster and compare with the original VCF raster.
-compute the RMSE between your predicted and the actual tree cover values (hint)
-are the differences between the predicted and actual tree cover the same for all of the 3 classes we used for the random forest classfication? Using the training polygons from the random forest classification, calculate the RMSE separately for each of the classes and compare. Hint - see ?zonal().
+rm(list=ls())
 
 library(raster)
+library(gridExtra)
 
-# load data
-load("data/GewataB1.rda")
-load("data/GewataB5.rda")
-load("data/GewataB7.rda")
+# Load band data and training polygons
+load("data/GewataB1.rda") 
+load("data/GewataB2.rda")
+load("data/GewataB3.rda")
+load("data/GewataB4.rda")
+load("data/GewataB5.rda") 
+load("data/GewataB7.rda") 
+load("data/vcfGewata.rda") #Vegetation Continuous Field
+load("data/trainingPoly.rda")
 
-load("data/vcfGewata.rda")
+# Remove outliers (Based on histogram and summary of each band)
+GewataB1[GewataB1 > 1000] <- NA
+GewataB2[GewataB2 > 1300] <- NA
+GewataB3[GewataB3 > 1500] <- NA
+GewataB5[GewataB5 > 4000] <- NA
+GewataB7[GewataB7 > 3000] <- NA
+
+# Remove water, clouds and shadows from VCF
 vcfGewata[vcfGewata > 100] <- NA
-plot(vcfGewata)
-summary(vcfGewata)
-hist(vcfGewata)
 
-# cell statistics (2 ways)
-cellStats(GewataB1, stat=mean)
-maxValue(GewataB7)
+# Combine bands in a rasterBrick and convert to original values
+gewata <- brick(GewataB1, GewataB2, GewataB3, GewataB4, GewataB5, GewataB7)
+gewata <- calc(gewata, fun=function(x) x / 10000)
 
-# summary for a quick overview
-summary(GewataB1)
-hist(GewataB7)
-summary(GewataB5)
-summary(GewataB7)
-
-# put the 3 bands into a rasterBrick
-gewata <- brick(GewataB1, GewataB5, GewataB7)
-
-# explore the brick
-hist(gewata)
-hist(gewata, xlim = c(0, 16000), ylim = c(0, 750000), breaks = seq(0, 16000, by = 100))
-opar <- par(mfrow = c(1, 1)) # reset plotting window
-pairs(gewata)
-
-# make new brick with covariates (combine Gewata with VCF)
+# Relationship between bands and VCF
 covs <- addLayer(gewata, vcfGewata)
-names(covs) <- c("Band1", "Band5", "Band7", "VCF")
-plot(covs)
-par(opar)
+names(covs) <- c("Band1", "Band2", "Band3", "Band4", "Band5", "Band7", "VCF")
+pairs(covs)
 
+# Band 3 and 7 have the highest correlation coefficient with the VCF (both 0.89). 
+# Surprisingly band 4 (NIR)has the lowest correlation coefficient with the VCF (0.17).
+
+# Linear model
+covsdf <- as.data.frame(covs, na.rm=T)
+model <- lm(VCF ~ Band1 + Band2 + Band3 + Band4 + Band5 + Band7, data = covsdf)
+summary(model)
+
+# In the Pr(>|t|), which represents the variable p-value, all bands show a very low value, 
+# which means that the chance that the band is meaningless for the model is very low. 
+# All bands have 3 stars (***), corresponding with a very good significance. For more 
+# information on this subject: http://blog.yhathq.com/posts/r-lm-summary.html
+
+# Prediction of tree cover based on the model
+vcf_predict <- predict(covs, model, na.rm=T)
+vcf_predict[vcf_predict < 0] <- NA
+
+# Plots of prediction
+grid.arrange(spplot(vcf_predict, zlim=c(0,100) ,col.regions=colorRampPalette(c("white", "darkkhaki", "darkgreen"))(100), main="Predicted tree cover"), spplot(vcfGewata, zlim=c(0,100) ,col.regions=colorRampPalette(c("white", "darkkhaki", "darkgreen"))(100), main="Tree cover VCF"), ncol=1)
+
+# Difference
+vcf_dif <- vcf_predict - vcfGewata
+spplot(vcf_dif, zlim = c(-60,60), main="Tree cover VCF", zcol="layer", col.regions=colorRampPalette(c("red", "yellow", "darkgreen"))(100))
+
+# Root mean squared Error between Predicted and actual tree cover
+dif_df <- as.data.frame(vcf_predict$Treecover - covs$VCF, na.rm=T)
+RMSE <- sqrt(mean(dif_df ^ 2))
+
+# Set up trainings data
+trainingPoly@data$Code <- as.numeric(trainingPoly@data$Class)
+
+# Assign "code" values to raster cells
+land_use_classes <- rasterize(trainingPoly, covs$VCF, field='Code')
+
+# Brick with predicted and actual tree cover
+pred_act_brick <- brick(covs$VCF, vcf_predict)
+pred_act_brick
+
+# Calculate the mean for every class by the use of zonal()
+mean_cover <- zonal(pred_act_brick, land_use_classes, fun='mean',na.rm=T)
+mean_cover_df <- as.data.frame(mean_cover)
+
+# Calculate the Root mean squared Error for all of the 3 classes
+rmse_mean_cover <- sqrt((mean_cover_df$VCF - mean_cover_df$layer)^2), decimals
+names(rmse_mean_cover) <- c("Crop", "Forest", "Wetlands")
+
+# Print statement
+print(paste("The rmse is different for the different landuses. Crop has a rmse of", 
+            rmse_mean_cover[1], "Forest has a rmse of", rmse_mean_cover[2], 
+            "and wetlands has a rmse for the prediction of", rmse_mean_cover[3]))
